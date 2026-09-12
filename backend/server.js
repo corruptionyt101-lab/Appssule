36f918beb4048a7581e69eb03d60ff55958ca508
corruptionyt101-lab <corruptionyt101@gmail.com>
2026-09-11 22:05:32 -0400

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,4 @@
-const express = require('express');
-const app = express();
-
-// Import your existing routes or app logic if you have an app.js
-// const appModule = require('./app');
-
-app.get('/', (req, res) => {
-  res.send('Backend is running on Vercel!');
-});
+const app = require('./app');
 
 if (process.env.NODE_ENV !== 'production') {
   const PORT = process.env.PORT || 3000;
